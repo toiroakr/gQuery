1e229397bade906a1c3d011c7bc59c91ca72ceb7
a-s--k-h <qazsewsxcd@gmail.com>
2015-08-06 00:51:03 +0900

    delete logger & gradle build setting

---
+++ src/main/java/com/toiroakr/gquery/JsonElementValuePair.java
@@ -0,0 +1,11 @@
+package com.toiroakr.gquery;
+
+import com.google.gson.JsonElement;
+
+/**
+ * Created by Akira on 15/08/05.
+ */
+public class JsonElementValuePair {
+    public Object valueObject;
+    public JsonElement jsonElement;
+}
